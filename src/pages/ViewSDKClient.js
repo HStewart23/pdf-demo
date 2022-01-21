2d6508b684927ee19157a6a9c008a794ff69ee2b
Hannah <hannah.stewart@ellenmacarthurfoundation.org>
2022-01-21 08:37:00 +0000

    trying to hook up tag manager

--- src/pages/ViewSDKClient.js
+++ src/pages/ViewSDKClient.js
@@ -58,33 +58,6 @@ const ViewNewSDKClient = () => {
       return previewFilePromise;
     }
 
-    // registerSaveApiHandler() {
-    //   /* Define Save API Handler */
-    //   const saveApiHandler = (metaData, content, options) => {
-    //     console.log(metaData, content, options);
-    //     return new Promise(resolve => {
-    //       /* Dummy implementation of Save API, replace with your business logic */
-    //       setTimeout(() => {
-    //         const response = {
-    //           code: window.AdobeDC.View.Enum.ApiResponseCode.SUCCESS,
-    //           data: {
-    //             metaData: Object.assign(metaData, {
-    //               updatedAt: new Date().getTime(),
-    //             }),
-    //           },
-    //         };
-    //         resolve(response);
-    //       }, 2000);
-    //     });
-    //   };
-
-    //   this.adobeDCView.registerCallback(
-    //     window.AdobeDC.View.Enum.CallbackType.SAVE_API,
-    //     saveApiHandler,
-    //     {}
-    //   );
-    // }
-
     registerEventsHandler() {
       /* Register the callback to receive the events */
       this.adobeDCView.registerCallback(
