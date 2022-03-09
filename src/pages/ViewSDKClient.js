9c16c44ba214581dd3267b01473a5a41c5b3f250
Hannah <hannah.stewart@ellenmacarthurfoundation.org>
2022-03-09 15:21:49 +0000

    more isBrowser checks

--- src/pages/ViewSDKClient.js
+++ src/pages/ViewSDKClient.js
@@ -1,61 +1,81 @@
 class ViewSDKClient {
   constructor() {
-    this.readyPromise = new Promise(resolve => {
-      if (window.AdobeDC) {
-        resolve();
-      } else {
-        document.addEventListener('adobe_dc_view_sdk.ready', () => {
+    const isBrowser = typeof window !== 'undefined';
+    if (!isBrowser) {
+      return null;
+    } else {
+      this.readyPromise = new Promise(resolve => {
+        if (window.AdobeDC) {
           resolve();
-        });
-      }
-    });
-    this.adobeDCView = undefined;
-    this.pdfEvent = [];
+        } else {
+          document.addEventListener('adobe_dc_view_sdk.ready', () => {
+            resolve();
+          });
+        }
+      });
+      this.adobeDCView = undefined;
+      this.pdfEvent = [];
+    }
   }
 
   ready() {
-    return this.readyPromise;
+    const isBrowser = typeof window !== 'undefined';
+    if (!isBrowser) {
+      return null;
+    } else {
+      return this.readyPromise;
+    }
   }
 
   previewFile(divId, viewerConfig) {
-    const config = {
-      clientId: 'b9eb10d4729b433cbb865c348412b92c',
-    };
-    if (divId) {
-      config.divId = divId;
-    }
+    const isBrowser = typeof window !== 'undefined';
+    if (!isBrowser) {
+      return null;
+    } else {
+      const config = {
+        clientId: 'b9eb10d4729b433cbb865c348412b92c',
+      };
+      if (divId) {
+        config.divId = divId;
+      }
 
-    this.adobeDCView = new window.AdobeDC.View(config);
+      this.adobeDCView = new window.AdobeDC.View(config);
 
-    const previewFilePromise = this.adobeDCView.previewFile(
-      {
-        content: {
-          location: {
-            url: 'https://emf.thirdlight.com/file/24/pN3kJWTpN_5hi8hpNordpNJo0VT/1.%20EMF_Africa_Built%20Env_Final-11Oct%20-%20En.pdf',
+      const previewFilePromise = this.adobeDCView.previewFile(
+        {
+          content: {
+            location: {
+              url: 'https://emf.thirdlight.com/file/24/pN3kJWTpN_5hi8hpNordpNJo0VT/1.%20EMF_Africa_Built%20Env_Final-11Oct%20-%20En.pdf',
+            },
+          },
+          metaData: {
+            fileName: 'Test.pdf',
+            id: 'b9eb10d4729b433cbb865c348412b92c',
           },
         },
-        metaData: {
-          fileName: 'Test.pdf',
-          id: 'b9eb10d4729b433cbb865c348412b92c',
-        },
-      },
-      viewerConfig
-    );
+        viewerConfig
+      );
 
-    return previewFilePromise;
+      return previewFilePromise;
+    }
   }
 
   registerEventsHandler(addEventToDataLayer) {
-    this.adobeDCView.registerCallback(
-      window.AdobeDC.View.Enum.CallbackType.EVENT_LISTENER,
-      event => {
-        addEventToDataLayer(event);
-      },
-      {
-        enablePDFAnalytics: true,
-      }
-    );
-    return this.pdfEvent;
+    const isBrowser = typeof window !== 'undefined';
+    if (!isBrowser) {
+      return null;
+    } else {
+      this.adobeDCView.registerCallback(
+        window.AdobeDC.View.Enum.CallbackType.EVENT_LISTENER,
+        event => {
+          addEventToDataLayer(event);
+        },
+        {
+          enablePDFAnalytics: true,
+        }
+      );
+      return this.pdfEvent;
+    }
   }
 }
 
